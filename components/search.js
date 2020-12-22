23a20deab3fefe78495b986564d71e4f0f0529dd
Lauren W <laurenwei0613@gmail.com>
2020-12-22 18:21:36 -0500

    Add Search function

--- components/search.js
+++ components/search.js
@@ -1,13 +1,158 @@
-import React from 'react';
-import { StyleSheet, Text, View , Dimensions} from 'react-native';
-import PaymentSuccess from "./element/payment"
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View , Dimensions, ScrollView, FlatList, TouchableOpacity, Modal,TouchableHighlight} from 'react-native';
+import { SearchBar,Card,Avatar, ListItem, Button } from 'react-native-elements';
+import {FlatListSlider} from 'react-native-flatlist-slider';
 import AppHeader from './element/header';
 export default function Search({navigation}) {
-    return (
+    const [search, setSearch] = useState('');
+    const [filteredData, setfilteredData] = useState([]);
+    const [mainData, setMainData] = useState([]);const [modalVisible, setModalVisible] = useState(false);
+    const [profile, setProfile] = useState({});
+    const [popupVisible, setpopupVisible] = useState(false);
+    const [followed, setFollowed] = useState("Follow");
+
+    const restaurants = require("../data/restaurantData").restaurantProfiles;
+    useEffect(() => {
+        setfilteredData([]);
+        setMainData(restaurants);
+    },[]);
+
+    const searchFilterFunction = (text) => {
+        if (text) {
+          const newData = mainData.filter(function (item) {
+            const itemData = item.name
+              ? item.name.toUpperCase()
+              : ''.toUpperCase();
+            const textData = text.toUpperCase();
+            return itemData.indexOf(textData) > -1;
+          });
+          setfilteredData(newData);
+          setSearch(text);
+        } else {
+          setfilteredData([]);
+          setSearch(text);
+        }
+      };
+    //{{ uri: {item.avatar} }}
+    const ItemView = ({ item }) => {
+        return (
+          // Flat List Item
+          <TouchableOpacity onPress={() => getItem(item)}>
+          <Card  containerStyle={styles.cardListStyle} >
+            <ListItem containerStyle={styles.profileStyle}>
+              <Avatar
+                rounded
+                size = "large"
+                source= {{ uri: item.avatar}}
+              />
+              <ListItem.Content>
+                <ListItem.Title style={{ color: 'rgba(0,0,0,0.7)', fontWeight: 'bold', fontSize: 25 }}>{item.name}</ListItem.Title>
+                <ListItem.Subtitle>{item.location}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          </Card>
+          </TouchableOpacity>
+        );
+      };
+    
+    const ItemSeparatorView = () => {
+        return (
+          // Flat List Item Separator
+          <View
+            style={{
+              height: 20,
+              width: '100%',
+              backgroundColor: '#FFFFFF',
+            }}
+          />
+        );
+      };
+    
+    const getItem = (item) => {
+        // Function for click on an item
+        setModalVisible(true);
+        setProfile(item);
+      };
+
+    return ( 
         
         <View style={styles.containerStyle}>
-            <AppHeader centerText = "payment"></AppHeader>
-        
+            <AppHeader centerText = "Search"></AppHeader>
+            <ScrollView style={styles.scrollContainerStyle}>
+                    <SearchBar
+                    placeholder="Search for restaurants"
+                    searchIcon={{ size: 24 }}
+                    onChangeText={(text) => searchFilterFunction(text)}
+                    onClear={(text) => searchFilterFunction('')}
+                    value={search}
+                    containerStyle = {styles.searchContainer}
+                    inputContainerStyle = {styles.inputContainer}
+                    />
+                    <FlatList
+                    style = {styles.list}
+                    data = {filteredData}
+                    keyExtractor = {(item,index) => index.toString()}
+                    //ItemSeparatorComponent = {ItemSeparatorView}
+                    renderItem = {ItemView}
+                    />
+                    <Modal
+                      animationType="none"
+                      transparent={true}
+                      visible={modalVisible}
+                      onRequestClose={() => {
+                        Alert.alert("Modal has been closed.");
+                      }}
+                    >
+                        <Card containerStyle={styles.cardStyle}>
+                        <View style = {{flexDirection: "column", alignItems:'flex-end'}}>
+                        <TouchableHighlight style={{ ...styles.openButton}}
+                            onPress={() => { setModalVisible(!modalVisible);}}>
+                        <Text style={styles.textStyle}>X</Text>
+                        </TouchableHighlight>
+                        </View>
+                         <View style={styles.cardTitleContainerStyle}>
+                         <Avatar
+                          rounded
+                          size = "large"
+                          source= {{ uri: profile.avatar}}/>
+                        <Card.Title style={styles.cardResStyle}>{profile.name}</Card.Title>
+                        <View style={{width: Dimensions.get("screen").width*0.05}}></View>
+
+                        <View style={{flex:1 , alignItems:"flex-end"}}>
+                            <Button buttonStyle={{alignSelf: "center", borderRadius: 10, marginTop: 20, backgroundColor: 'white'}} 
+                             titleStyle={styles.cardDetailStyle} title={followed}onPress = {() => {
+                                setpopupVisible(true);
+                                setFollowed("Following");
+                            }} />
+                        </View>
+                      </View>
+                      <View style={styles.cardInfoContainerStyle}>
+                      <View style={{flexDirection: "column"}}>
+                        <Text style = {{fontWeight:'bold', marginBottom:7}}>{profile.location}
+                        </Text>
+                        <FlatListSlider data = {profile.images}/>
+                      </View>
+                    <View style={{flex:1 , alignItems:"flex-end", paddingTop: 5}}>
+                    </View>
+                    </View>
+
+                    <Modal animationType="slide" transparent={true} visible={popupVisible}
+                        onRequestClose={() => {
+                        Alert.alert("Modal has been closed.");}}>
+                        <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <Text style={styles.modalText}>You will now receive notification from the restaurant!</Text>
+                                <TouchableHighlight style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+                                    onPress={() => {
+                                    setpopupVisible(!popupVisible);}}>
+                                    <Text style={styles.textStyle}>GOT IT</Text>
+                                </TouchableHighlight>
+                        </View>
+                        </View>
+                    </Modal>
+                    </Card>
+                  </Modal>
+            </ScrollView>
         </View>
     );
   }
@@ -15,4 +160,131 @@ const styles = StyleSheet.create({
     containerStyle:{
         flex:1
     },
+    profileStyle:{
+        backgroundColor: "transparent",
+        paddingHorizontal: 0,
+        paddingVertical:0
+    },
+    cardListStyle:{
+        marginHorizontal:0,
+         backgroundColor: '#F6C440',
+         borderRadius:10,
+         shadowColor: "rgba(0,0,0, 0.3)",
+         shadowOffset:{  width: 5,  height: 5 },
+         shadowOpacity: 1.0,
+     },
+    list:{
+       overflow: 'visible',
+     },
+    scrollContainerStyle:{
+        paddingHorizontal: 15,
+        overflow:'visible'
+    },
+    itemStyle: {
+        padding: 10,
+    },
+    searchContainer:{
+        backgroundColor: '#F6C440',
+        justifyContent: 'center',
+        paddingHorizontal: 15,
+        padding: 0,
+        borderRadius:10,
+        shadowOffset:{  width: 5,  height: 5 },
+        shadowColor: "rgba(0,0,0, 0.3)",
+        shadowOpacity: 1.0,
+        height: Dimensions.get("window").height * 0.12,
+        borderBottomColor: 'transparent',
+        borderTopColor: 'transparent',
+    },
+    inputContainer:{
+        backgroundColor: '#FFFFFF',
+        borderRadius:10,
+        alignItems:'center', 
+        justifyContent:'center',
+        height: Dimensions.get("window").height * 0.075,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+      },
+      modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+      },
+      openButton: {
+        backgroundColor: "black" ,
+        borderRadius: 10,
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        marginRight: 10,
+        marginTop: 10,
+      },
+      textStyle: {
+        fontSize:20,
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+      },
+      modalText: {
+        marginBottom: 15,
+        textAlign: "center",
+        fontSize: 20
+      },
+       cardStyle:{
+          marginTop: Dimensions.get("window").height * 0.14,
+            flexDirection: 'column',
+            backgroundColor: '#F6C440',
+            padding: 0,
+            borderRadius:10,
+            shadowColor: "rgba(0,0,0, 0.3)",
+            shadowOffset:{  width: 5,  height: 5 }
+        },
+        cardTitleContainerStyle:{
+            flexDirection: 'row',
+            padding: 12,
+            paddingBottom: 0,
+            paddingTop:0
+        },
+        cardResStyle:{
+            alignItems: 'flex-start',
+            fontSize: 30,
+            marginTop: 15,
+            marginLeft: 15,
+            alignSelf: 'center'
+        },
+        cardDetailButtonStyle:{
+            backgroundColor: "white",
+            alignSelf: "center",
+            borderRadius: 10,
+            marginTop: 20,
+        },
+        cardDetailStyle:{
+            color:"#000000",
+            fontWeight: 'bold',
+            marginHorizontal: 8,
+            fontSize: 15
+        },
+        cardFoodStyle:{
+            fontSize: 15,
+            marginTop: 10,
+        },
+        cardInfoContainerStyle:{
+            flexDirection:"row",
+            padding: 12,
+        },
 });
---
+++ data/restaurantData.js
@@ -0,0 +1,79 @@
+var restaurantProfiles = [
+    {
+        name: "Lousea",
+        id: 1,
+        location: "Taipei, Taiwan",
+        avatar: "https://cdn.dribbble.com/users/2898742/screenshots/5660488/logo.jpg",
+        images : [
+            {
+                image: "https://upload.wikimedia.org/wikipedia/commons/4/4b/Coffee_break_%283457656569%29.jpg",
+                desc: "test"
+            },
+            {
+                image: "https://upload.wikimedia.org/wikipedia/commons/4/46/Latte_art_cappuccino.jpg",
+                desc: "try"
+            }
+        ]
+    },
+    {   name: "Loutea",
+        id: 2,
+        location: "Taipei, Taiwan",
+        avatar: "https://i.redd.it/cbj106i8yzb21.png",
+        images : [
+            {
+                image: "https://i3.achangpro.com/img.tenjo.tw/uploads/20180222001246_12.jpg",
+                desc: "test"
+            },
+            {
+                image: "https://im.marieclaire.com.tw/m800c533h100b0/assets/mc/202001/5E34729875BD81580495512.jpeg",
+                desc: "try"
+            }
+        ]
+    },
+    {   name: "Lousia",
+        id: 3,
+        location: "Tainan, Taiwan",
+        avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTC_Zz5Fx_odT3BudujtgAAeUB1kkUKbM5Dg&usqp=CAU",
+        images : [
+            {
+                image: "https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG",
+                desc: "test"
+            },
+            {
+                image: "https://images.squarespace-cdn.com/content/v1/50437f72c4aa994481345d33/1598388294901-Z1YG0UTS5J5839MVHKSZ/ke17ZwdGBToddI8pDm48kPqQfq0L3n3wpHIsRapTfg8UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKczo5Zn4xktlpMsMj-QlHXeMfNK6GwvtVkYEWiR8XAPyD3GfLCe_DXOSC_YcAacWL_/Latte+Art.jpg?format=1500w",
+                desc: "try"
+            }
+        ]
+    },
+    {   name: "Pizza H",
+        id: 4,
+        location: "Taichung, Taiwan",
+        avatar: "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/bf5ae142231279.57c5b88d02e1a.png",
+        images : [
+            {
+                image: "https://doqvf81n9htmm.cloudfront.net/data/alicelee_126/201804/0416/54.jpg",
+                desc: "test"
+            },
+            {
+                image: "https://tokyo-kitchen.icook.network/uploads/category/background/112/e2919e9cfb5832b5.jpg",
+                desc: "try"
+            }
+        ]
+    },
+    {   name: "Tofu H.",
+        id: 5,
+        location: "Taipei, Taiwan",
+        avatar: "https://sribu-sg.s3.amazonaws.com/assets/media/contest_detail/2011/7/logo-restoran-tofu-house-4fac04ab59aa92040e0085da/20110707073506_1103.jpg",
+        images : [
+            {
+                image: "https://images.japancentre.com/recipes/pics/241/main/photo_Korean-Style-Kimuchi-Nabe-Hot-Pot.jpg?1469572978",
+                desc: "test"
+            },
+            {
+                image: "https://www.maangchi.com/wp-content/uploads/2019/06/kimchistewtuna.jpg",
+                desc: "try"
+            }
+        ]
+    },
+]
+exports.restaurantProfiles = restaurantProfiles;
